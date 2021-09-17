8bf3e5c9880f7ae0d231bd126df771a85c680a46
Craig H <craig.harrigan@snhu.edu>
2021-09-17 08:07:50 -0400

    removed extra folders, updated the routes per feedback

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+const controller = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', controller.index);
 
 module.exports = router;
