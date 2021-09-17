2693d99cc52f04a0430ab884b09108aa98ebd136
Craig H <craig.harrigan@snhu.edu>
2021-09-17 09:52:54 -0400

    Converted the Travel page from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
 /* GET travel view */
 const travel = (req, res) => {
     console.log(req.path);
     //NOT WORKIGN pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: 'Travlr Getaways - Travel' });
+    res.render('travel', {title: 'Travlr Getaways - Travel', trips });
 };
 
 module.exports = {
