6f5f716f899a6749b42cf654e321f38ab92c1baa
Craig H <craig.harrigan@snhu.edu>
2021-10-14 09:09:28 -0400

    added 401 handler in app.js

--- app.js
+++ app.js
@@ -47,6 +47,15 @@ app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
+//global unauthorized error handler (401)
+app.use(function(err, res, next){
+  if (err.name === 'UnauthorizedError'){
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
